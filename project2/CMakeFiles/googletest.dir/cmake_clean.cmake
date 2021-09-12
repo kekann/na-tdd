1c727c70ba37eba279aafd9deab72c9ea79bddd4
kekann <79042564+kekann@users.noreply.github.com>
2021-09-12 12:46:51 +0200

    Add files via upload
    ciekawe czy to sie w ogole polaczy i dalej bedzie dzialac xddd anyway czas na nauke mockow

---
+++ project2/CMakeFiles/googletest.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/googletest"
+  "CMakeFiles/googletest-complete"
+  "external/gtest/src/googletest-stamp/googletest-build"
+  "external/gtest/src/googletest-stamp/googletest-configure"
+  "external/gtest/src/googletest-stamp/googletest-download"
+  "external/gtest/src/googletest-stamp/googletest-install"
+  "external/gtest/src/googletest-stamp/googletest-mkdir"
+  "external/gtest/src/googletest-stamp/googletest-patch"
+  "external/gtest/src/googletest-stamp/googletest-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/googletest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- project2/uts/playerTest.cpp
+++ project2/uts/playerTest.cpp
@@ -1,23 +1,25 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
-
+#include "field.hpp"
 #include "player.hpp"
+
+
 
 class PlayerTest : public ::testing::Test
 {
-    protected:
-    PlayerTest() {};
-    ~PlayerTest() override {};
-
-    void SetUp() override {};
-    void TearDown() override {};
 };
 
 
-TEST_F(PlayerTest, a)
+TEST_F(PlayerTest, getEqualWhileCheckingIfPlayerSymbolIsCorrectlyX)
 {
+    std::unique_ptr<PlayerI> sut = std::make_unique<Player>();
+    sut->setPlayerSymbol(Field::X);
+    EXPECT_EQ(Field::X, sut->getPlayerSymbol());
 }
 
-TEST_F(PlayerTest, b)
+TEST_F(PlayerTest, getNotEqualWhileCheckingIfPlayerSymbolIsDefaultXorY)
 {
+    std::unique_ptr<PlayerI> sut = std::make_unique<Player>();
+    EXPECT_NE(Field::X, sut->getPlayerSymbol());
+    EXPECT_NE(Field::O, sut->getPlayerSymbol());
 }
